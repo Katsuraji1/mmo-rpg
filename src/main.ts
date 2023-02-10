d146d2c513c80f00883d9097301eb26969c7127e
Katsuraji1 <verahovets@gmail.com>
2023-02-10 19:54:02 +0300

    fix: fix builder

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import HelloWorldScene from './HelloWorldScene'
 
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
+	parent: 'app',
 	width: 800,
 	height: 600,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
-		}
+			gravity: { y: 200 },
+		},
 	},
-	scene: [HelloWorldScene]
+	scene: [HelloWorldScene],
 }
 
 export default new Phaser.Game(config)
